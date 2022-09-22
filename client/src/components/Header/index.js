cbb57c2c65bbeda17b894fe0c5f5e12cb86f954d
bereky <bereketlemma4@gmail.com>
2022-09-22 09:54:19 +0300

    header component created

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,11 @@
 import './index.css'
+import Nav from './Nav';
 
 const Header = () => {
     return ( 
         <div className="header">
             <div className="header-container">
-                
+                <Nav />
             </div>
         </div>
      );
