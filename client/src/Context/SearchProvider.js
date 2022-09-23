dce5d3823df0d82b9299352440a38280874604e3
bereky <bereketlemma4@gmail.com>
2022-09-23 12:13:07 +0300

    search context added

---
+++ client/src/Context/SearchProvider.js
@@ -0,0 +1,27 @@
+import { useContext, useState } from "react"
+import { SearchContext } from "./SearchContext"
+
+export const SearchProvider = ({ children }) => {
+    const [ open, setOpen] = useState()
+
+    const setIsOpen = () => {
+        console.log("update")
+        if(open){
+            setOpen(false)
+        }
+        else {
+            setOpen(true)
+        }
+    }
+
+    const searchCxt = {
+        searchBarIsOpen: open,
+        setSearchBarIsOpen: setIsOpen
+    }
+
+    return(
+        <SearchContext.Provider value={searchCxt}>
+            {children}
+        </SearchContext.Provider>
+    ) 
+}
