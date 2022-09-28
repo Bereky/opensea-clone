ac743a8d7cef8ed20585715f39160179f01f1e2d
bereky <bereketlemma4@gmail.com>
2022-09-28 08:47:05 +0300

    added night mode toggle icon

--- client/src/components/Header/NavSide.js
+++ client/src/components/Header/NavSide.js
@@ -1,6 +1,11 @@
 
 import ExploreIcon from '@mui/icons-material/Explore';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import LanguageIcon from '@mui/icons-material/Language';
+import Brightness2Icon from '@mui/icons-material/Brightness2';
+import ToggleOffIcon from '@mui/icons-material/ToggleOff';
 
 const NavSide = () => {
     return (
@@ -14,13 +19,71 @@ const NavSide = () => {
                                 <div className="menu-item-icon-main">
                                     <ExploreIcon fontSize="large" />
                                 </div>
-                                    Explore
+                                Explore
                                 <div className="menu-item-icon-end">
                                     <KeyboardArrowRightIcon fontSize="large" />
                                 </div>
                                 </button>
                             </div>
                         </li>
+
+                        <li className="nav-mobile-menu-item">
+                            <div className="menu-item-container">
+                                <button className="menu-item-button">
+                                <div className="menu-item-icon-main">
+                                    <LeaderboardIcon fontSize="large" />
+                                </div>
+                                    Stats
+                                <div className="menu-item-icon-end">
+                                    <KeyboardArrowRightIcon fontSize="large" />
+                                </div>
+                                </button>
+                            </div>
+                        </li>
+
+                        <li className="nav-mobile-menu-item">
+                            <div className="menu-item-container">
+                                <button className="menu-item-button">
+                                <div className="menu-item-icon-main">
+                                    <LibraryBooksIcon fontSize="large" />
+                                </div>
+                                    Resources
+                                <div className="menu-item-icon-end">
+                                    <KeyboardArrowRightIcon fontSize="large" />
+                                </div>
+                                </button>
+                            </div>
+                        </li>
+
+                        <li className="nav-mobile-menu-item">
+                            <div className="menu-item-container">
+                                <button className="menu-item-button">
+                                <div className="menu-item-icon-main">
+                                    <LanguageIcon fontSize="large" />
+                                </div>
+                                    Language
+                                <div className="menu-item-icon-end">
+                                    <KeyboardArrowRightIcon fontSize="large" />
+                                </div>
+                                </button>
+                            </div>
+                        </li>
+
+                        <li className="nav-mobile-menu-item">
+                            <div className="menu-item-container">
+                                <button className="menu-item-button">
+                                <div className="menu-item-icon-main">
+                                    <Brightness2Icon fontSize="large" />
+                                </div>
+                                    Night Mode
+                                <div className="menu-item-icon-end">
+                                    <ToggleOffIcon fontSize="large" />
+                                </div>
+                                </button>
+                            </div>
+                        </li>
+
+                        
                     </ul>
                 </div>
             </div>
