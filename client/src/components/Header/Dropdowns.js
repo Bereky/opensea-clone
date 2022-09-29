b12220bb131d18e776a14c2556ce794857685eee
bereky <bereketlemma4@gmail.com>
2022-09-29 22:31:25 +0300

    add dropdown list

---
+++ client/src/components/Header/Dropdowns.js
@@ -0,0 +1,55 @@
+
+export const Dropdowns = {
+    explore: [
+        {
+            name: "All NFTs",
+            iconsrc: "https://opensea.io/static/images/icons/allnfts-light.svg",
+            altText: "all-nfts"
+        },
+        {
+            name: "Solana NFTs",
+            iconsrc: "https://opensea.io/static/images/icons/solana-light.svg",
+            altText: "solana nft"
+        },
+        {
+            name: "Art",
+            iconsrc: "https://opensea.io/static/images/icons/art-light.svg",
+            altText: "art"
+        },
+        {
+            name: "Art",
+            iconsrc: "https://opensea.io/static/images/icons/collectibles-light.svg",
+            altText: "collectibles"
+        },
+        {
+            name: "Domain Names",
+            iconsrc: "https://opensea.io/static/images/icons/domain-names-light.svg",
+            altText: "domain name"
+        },
+        {
+            name: "Music",
+            iconsrc: "https://opensea.io/static/images/icons/music-light.svg",
+            altText: "music"
+        },
+        {
+            name: "Photography",
+            iconsrc: "https://opensea.io/static/images/icons/photography-category-light.svg",
+            altText: "photography"
+        },
+        {
+            name: "Trading Cards",
+            iconsrc: "https://opensea.io/static/images/icons/sports-light.svg",
+            altText: "trading cards"
+        },
+        {
+            name: "Utility",
+            iconsrc: "https://opensea.io/static/images/icons/utility-light.svg",
+            altText: "utility"
+        },
+        {
+            name: "Virtual Worlds",
+            iconsrc: "https://opensea.io/static/images/icons/virtual-worlds-light.svg",
+            altText: "virtual world"
+        }
+    ]
+}
