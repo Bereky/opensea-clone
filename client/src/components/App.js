2b8d03c4c1acaf02b29e259aaa97ef7c77ab0669
bereky <bereketlemma4@gmail.com>
2022-10-02 18:33:45 +0300

    removed wrapper context

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,8 @@
-import { Fragment } from 'react'
-import { SearchProvider } from '../Context/SearchProvider';
 import Header from './Header';
 
 const App = () => {
     return ( 
-        <SearchProvider>
             <Header />
-        </SearchProvider>
      );
 }
  
